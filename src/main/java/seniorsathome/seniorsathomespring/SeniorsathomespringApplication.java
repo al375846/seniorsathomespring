9509bc81d59970a7ec09bfda6c89a51e94a715c1
al375846 <al375846@uji.es>
2020-02-27 09:33:52 +0100

    Initial commit

---
+++ src/main/java/seniorsathome/seniorsathomespring/SeniorsathomespringApplication.java
@@ -0,0 +1,13 @@
+package seniorsathome.seniorsathomespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeniorsathomespringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeniorsathomespringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/seniorsathome/seniorsathomespring/SeniorsathomespringApplicationTests.java
@@ -0,0 +1,13 @@
+package seniorsathome.seniorsathomespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeniorsathomespringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
