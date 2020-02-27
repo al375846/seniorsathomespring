2e5a7633700f6e24a22d8c7391ba217474fb1c73
iscervera <iscervera.tirado@gmail.com>
2020-02-27 11:49:58 +0100

    Prueba 2

--- src/main/java/seniorsathome/seniorsathomespring/SeniorsathomespringApplication.java
+++ src/main/java/seniorsathome/seniorsathomespring/SeniorsathomespringApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeniorsathomespringApplication {
 
-    private String prueba;
+    private String prueba_isa;
 
     public static void main(String[] args) {
         SpringApplication.run(SeniorsathomespringApplication.class, args);
