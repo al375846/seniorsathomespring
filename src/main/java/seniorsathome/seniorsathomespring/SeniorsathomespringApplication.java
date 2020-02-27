5b9eaad63f79b48424705c66822eaddd36ec2f14
CrisRubio <al375866@uji.es>
2020-02-27 10:48:17 +0100

    Prueba 1

--- src/main/java/seniorsathome/seniorsathomespring/SeniorsathomespringApplication.java
+++ src/main/java/seniorsathome/seniorsathomespring/SeniorsathomespringApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SeniorsathomespringApplication {
 
+    private String prueba;
+
     public static void main(String[] args) {
         SpringApplication.run(SeniorsathomespringApplication.class, args);
     }
