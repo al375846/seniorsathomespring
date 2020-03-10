774b3864fcdf52e4a33103640e7e6619189da297
al375846 <al375846@uji.es>
2020-03-10 19:48:38 +0100

    tutoria 1

--- src/main/java/seniorsathome/seniorsathomespring/SeniorsathomespringApplication.java
+++ src/main/java/seniorsathome/seniorsathomespring/SeniorsathomespringApplication.java
@@ -2,12 +2,20 @@ package seniorsathome.seniorsathomespring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class SeniorsathomespringApplication {
 
+    private static final Logger log = Logger.getLogger(SeniorsathomespringApplication .class.getName());
     public static void main(String[] args) {
-        SpringApplication.run(SeniorsathomespringApplication.class, args);
+        new SpringApplicationBuilder(SeniorsathomespringApplication .class).run(args);
     }
+
+    /*public static void main(String[] args) {
+        SpringApplication.run(SeniorsathomespringApplication.class, args);
+    }*/
 
 }
