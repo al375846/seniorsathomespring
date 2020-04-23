1781d8031c34acc2775533aaa1a3f4a7fd52c54b
al375846 <al375846@uji.es>
2020-04-23 13:52:39 +0200

    login popup funcionando todo

--- src/main/java/seniorsathome/seniorsathomespring/controller/LoginController.java
+++ src/main/java/seniorsathome/seniorsathomespring/controller/LoginController.java
@@ -7,10 +7,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import seniorsathome.seniorsathomespring.dao.UserDao;
 import seniorsathome.seniorsathomespring.model.User;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class LoginController {
@@ -50,6 +53,28 @@ public class LoginController {
 
         // Torna a la pàgina principal
         return "redirect:/";
+    }
+
+    @RequestMapping(value="/loginpopup", method= RequestMethod.POST)
+    public String checkPopupLogin(User user, HttpSession session){
+        if (user.getPassword().trim().equals("") || user.getUsername().trim().equals("")) {
+            return "redirect:/login";
+        }
+        user = userDao.loadUserByUsername(user.getUsername(), user.getPassword());
+        if (user == null) {
+            return "redirect:/login";
+        }
+
+        session.setAttribute("user", user);
+
+        if (session.getAttribute("nextUrl") != null) {
+            String url = session.getAttribute("nextUrl").toString();
+            session.removeAttribute("nextUrl");
+            return "redirect:" + url;
+        }
+
+        return "redirect:/";
+
     }
 
     @RequestMapping("/logout")
