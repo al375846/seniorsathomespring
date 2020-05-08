1fb83f16127811d0bfc665295dd186c161cbc6ac
al375840 <47137539+al375840@users.noreply.github.com>
2020-05-08 12:50:54 +0200

    Registro 1.1

--- src/main/java/seniorsathome/seniorsathomespring/dao/BeneficiaryDao.java
+++ src/main/java/seniorsathome/seniorsathomespring/dao/BeneficiaryDao.java
@@ -28,7 +28,7 @@ public class BeneficiaryDao {
         int numero = conseguirNumero();
         BasicPasswordEncryptor passwordEncryptor = new BasicPasswordEncryptor();
         jdbcTemplate.update("INSERT INTO Beneficiary VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)",
-                "B"+numero, beneficiary.getName(), beneficiary.getSurnames(), beneficiary.getPhoneNumber(), beneficiary.getEmail(), beneficiary.getAddress(), beneficiary.getUserName(), passwordEncryptor.encryptPassword(beneficiary.getPassword()),"");
+                "B" + numero, beneficiary.getName(), beneficiary.getSurnames(), beneficiary.getPhoneNumber(), beneficiary.getEmail(), beneficiary.getAddress(), beneficiary.getUserName(), passwordEncryptor.encryptPassword(beneficiary.getPassword()), "");
     }
 
     /*Elimina un beneficiario a la base de datos */
@@ -60,7 +60,7 @@ public class BeneficiaryDao {
     /*Obtiene una lista de todos los beneficiarios de la base de datos. Devuelve una lista vacia si no hay*/
     public List<Beneficiary> getBeneficiaries() {
         try {
-            return jdbcTemplate.query("SELECT * FROM Beneficiary WHERE identification_number<>'' ORDER BY identification_number " ,
+            return jdbcTemplate.query("SELECT * FROM Beneficiary WHERE identification_number<>'' ORDER BY identification_number ",
                     new BeneficiaryRowMapper());
         } catch (EmptyResultDataAccessException e) {
             return new ArrayList<Beneficiary>();
@@ -77,40 +77,42 @@ public class BeneficiaryDao {
         }
     }
 
-    public String countRequests(){
-        try{
+    public String countRequests() {
+        try {
             Integer numero = jdbcTemplate.queryForObject("SELECT COUNT(*) FROM Request", Integer.class);
-            Integer añadir = numero+1;
-            return "R"+añadir.toString();
-        }catch(EmptyResultDataAccessException e){
+            Integer añadir = numero + 1;
+            return "R" + añadir.toString();
+        } catch (EmptyResultDataAccessException e) {
             return null;
         }
 
     }
 
-    public void addRequest(Request request){
+    public void addRequest(Request request) {
         jdbcTemplate.update("INSERT INTO Request VALUES(?, ?::STATUSTYPE, ?::SERVICETYPE, ?, ?, ?, ?, ?, ?, ?)",
                 countRequests(), "UNSOLVED", request.getType(), request.getStart_date(),
                 request.getFinal_date(), null, null, request.getComments(),
                 request.getBeneficiary_id(), "");
     }
 
-    public List<Request> activeServices(String identificationNumber){
+    public List<Request> activeServices(String identificationNumber) {
         return jdbcTemplate.query("SELECT * FROM Request WHERE beneficiary_id=? AND start_date<=current_date AND final_date>=current_date AND status='APPROVED'", new RequestRowMapper(), identificationNumber);
     }
-    public int conseguirNumero(){
-        List<Beneficiary>lista =getBeneficiaries();
-/*        int numero_anterior = Integer.parseInt(lista.get(0).getIdentificationNumber().split("B")[0]);
-        for (int i = 1; i < lista.size(); i++) {
 
-            int numero_actual = Integer.parseInt(lista.get(i).getIdentificationNumber().split("B")[0]);
+    public int conseguirNumero() {
+        List<Beneficiary> lista = getBeneficiaries();
+        int numero_anterior = Integer.parseInt(lista.get(0).getIdentificationNumber().split("b")[1]);
+        for (int i = 1; i < lista.size() - 1; i++) {
+
+            int numero_actual = Integer.parseInt(lista.get(i).getIdentificationNumber().split("b")[1]);
 
             if (numero_actual - numero_anterior == 2) {
                 return numero_actual - 1;
             }
 
             numero_anterior = numero_actual;
-        }*/
-        return lista.size()+1;
+        }
+
+        return lista.size() + 1;
     }
 }
