ced6a8f6b3383ed41b3e54a07e50966e51f68fb1
CrisRubio <al375866@uji.es>
2020-05-31 21:51:41 +0200

    Diseño cris acabado

--- src/main/java/seniorsathome/seniorsathomespring/controller/CompanyValidator.java
+++ src/main/java/seniorsathome/seniorsathomespring/controller/CompanyValidator.java
@@ -21,7 +21,20 @@ public class CompanyValidator implements Validator {
             errors.rejectValue("responsibleName", "Required", "You must enter a responsible name");
         if (company.getPassword().trim().equals(""))
             errors.rejectValue("password", "Required", "You must enter a password");
+        if (company.getName().trim().equals(""))
+            errors.rejectValue("name", "Required", "You must enter a name");
+        if (company.getEmail().trim().equals(""))
+            errors.rejectValue("email", "Required", "You must enter an email");
+        if (company.getResponsiblePhoneNumber().trim().equals(""))
+            errors.rejectValue("responsiblePhoneNumber", "Required", "You must enter a phone number from the responsible");
+        if (company.getResponsibleAddress().trim().equals(""))
+            errors.rejectValue("responsibleAddress", "Required", "You must enter an address");
+        if (company.getPhoneNumber().trim().equals(""))
+            errors.rejectValue("phoneNumber", "Required", "You must enter a phone number");
+
         if (company.getPassword().length() <= 4)
             errors.rejectValue("password", "Incorrect Value", "The password must have more than 4 characters");
+        if (company.getPassword().trim().length() > 40)
+            errors.rejectValue("password", "Incorrect Value", "You must enter a password with less than 40 characters");
     }
 }
