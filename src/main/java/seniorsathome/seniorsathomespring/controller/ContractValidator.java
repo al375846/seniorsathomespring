a91ecbf2eb858fa1be7d74c06be89ee06fd68bf2
CrisRubio <al375866@uji.es>
2020-06-03 11:34:24 +0200

    casManager y casVolunteer acabados

--- src/main/java/seniorsathome/seniorsathomespring/controller/ContractValidator.java
+++ src/main/java/seniorsathome/seniorsathomespring/controller/ContractValidator.java
@@ -4,6 +4,8 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 import seniorsathome.seniorsathomespring.model.Contract;
 
+import java.time.LocalDate;
+
 
 public class ContractValidator implements Validator {
     @Override
@@ -14,20 +16,29 @@ public class ContractValidator implements Validator {
     @Override
     public void validate(Object obj, Errors errors) {
         Contract contract = (Contract)obj;
-        /*if (contract.getNumberID().trim().equals(""))
-            errors.rejectValue("numberID", "Required", "You must enter an id");*/
+
         if (contract.getServiceType().trim().equals(""))
             errors.rejectValue("serviceType", "Required", "You must enter a service type");
+
         if (contract.getQuantity() <= 0)
             errors.rejectValue("quantity", "Incorrect Value", "The company must offer at least one service");
+
         if (contract.getStartDate() == null)
             errors.rejectValue("startDate", "Required", "You must enter a start date");
+        if (contract.getStartDate().isBefore(LocalDate.now()) || contract.getStartDate().equals(LocalDate.now()))
+            errors.rejectValue("startDate", "Incorrect Value", "You must enter a later date");
+
         if (contract.getFinalDate() == null)
             errors.rejectValue("finalDate", "Required", "You must enter a final date");
+
         if (contract.getFinalDate() != null && contract.getStartDate() != null && contract.getFinalDate().isBefore(contract.getStartDate()))
             errors.rejectValue("finalDate", "Incorrect Value", "The final date must be after the start date");
+
         if (contract.getCompanyID().trim().equals(""))
             errors.rejectValue("companyID", "Required", "You must enter a company name");
+
+
+
     }
 }
 
